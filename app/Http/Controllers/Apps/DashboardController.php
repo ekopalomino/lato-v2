9fa868aae510669f6ddbe1da6ece79dede9c36a6
ehw <heru.palomino@gmail.com>
2024-01-09 22:24:31 +0700

    Update

--- app/Http/Controllers/Apps/DashboardController.php
+++ app/Http/Controllers/Apps/DashboardController.php
@@ -4,6 +4,8 @@ namespace iteos\Http\Controllers\Apps;
 
 use Illuminate\Http\Request;
 use iteos\Http\Controllers\Controller;
+use iteos\Models\Product;
+use iteos\Models\Inventory;
 use DB;
 use Carbon\Carbon;
 
@@ -11,7 +13,9 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        
-        return view('apps.pages.dashboard');
+        /*Top Widget */
+        $products = Product::where('deleted_at',NULL)->count();
+        $bogorStocks = Inventory::where('branch_id','1')->sum('closing_amount');
+        return view('apps.pages.dashboard',compact('products','bogorStocks'));
     }
 }
--- resources/views/apps/pages/dashboard.blade.php
+++ resources/views/apps/pages/dashboard.blade.php
@@ -8,7 +8,34 @@ LATO | Dashboard
 @endsection
 @section('content')
 <div class="page-content">
-	
+    <div class="row">
+		<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+			<a class="dashboard-stat dashboard-stat-v2 blue" href="#">
+                <div class="visual">
+                    <i class="fa fa-comments"></i>
+                </div>
+                <div class="details">
+                    <div class="number">
+                        <span data-counter="counterup" data-value="{{ $products }}">{{ $products }}</span>
+                    </div>
+                    <div class="desc"> Total Product </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+			<a class="dashboard-stat dashboard-stat-v2 red" href="#">
+                <div class="visual">
+                    <i class="fa fa-comments"></i>
+                </div>
+                <div class="details">
+                    <div class="number">
+                        <span data-counter="counterup" data-value="{{ $bogorStocks }}">{{ $bogorStocks }}</span>
+                    </div>
+                    <div class="desc"> Bogor </div>
+                </div>
+            </a>
+        </div>
+    </div>
 </div>
 @endsection
 @section('footer.plugins')
